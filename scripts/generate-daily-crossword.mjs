cd46f6ecd09a7a60a2578d410adf4bb1cea2c7b1
dtfgenetics <green801028@gmail.com>
2026-06-28 11:14:42 -0500

    Wire daily generator imports

--- scripts/generate-daily-crossword.mjs
+++ scripts/generate-daily-crossword.mjs
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import { validateClueBank } from '../src/crossword/validate.js';
+import { exportIpuz, exportExolve } from '../src/crossword/exporters.js';
+import { buildDailyPuzzle } from '../src/crossword/generatePuzzle.js';
+import { currentDailyDate } from '../src/crossword/date.js';
+import { writeDailyArchiveIndex } from './build-daily-index.mjs';
+
 console.log('Daily crossword generator ready.');
